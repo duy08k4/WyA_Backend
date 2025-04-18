8af4e4642dbf2ba40d7f7544da1879f1f6ee85c0
duyo8k4 <duytran.290804@gmail.com>
2025-04-19 00:38:01 +0700

    Done: Register Function

---
+++ src/models/verifyOtp.model.js
@@ -0,0 +1,23 @@
+const verifyOtp_Model = (req, res) => {
+    const getOtp = req.cookies.otp
+    const data = req.body.data
+    const getInputOtp = data.inputOtp
+
+    if (getInputOtp == getOtp) {
+        return {
+            status: 200,
+            data: {
+                mess: "Verified"
+            }
+        }
+    }
+
+    return {
+        status: 400,
+        data: {
+            mess: "OTP code is incorrect"
+        }
+    }
+}
+
+module.exports = verifyOtp_Model
